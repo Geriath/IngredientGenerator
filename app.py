f86c76224c1e9d449064f8a631cc4ea29c5dc394
Geriath <34312983+Geriath@users.noreply.github.com>
2023-02-14 20:55:34 +0100

    IT WORKS AGAIN

--- app.py
+++ app.py
@@ -9,21 +9,28 @@ def basic():
     response = openai.Completion.create(
     model="text-davinci-003",
     prompt=generate_prompt(),
-    temperature=0.75,
     temperature=0.8,
-    max_tokens=300,
+    max_tokens=500,
 )
     result = response.choices[0].text
-    f = open("test.txt", "a")
+    splitresult = result.splitlines()
+    splitstring = splitresult[0]
+    bonk = len(splitstring)
+    name = splitstring[6:bonk]
+    f = open("Results/"+name+".txt", "a")
     f.write(result)
     f.close()
     return result
 
 def generate_prompt():
     return """Generate a fictional fantasy alchemy ingredient based on the real world plants, animals, fungi or minerals. 
-Name: (The name for the ingredient) 
-Latin Name: (The name of the ingredient in latin)
+Name: (The name of the ingredient) 
+Latin Name: (The name of the ingredient in Latin) 
 Description: (What it looks like) 
+Element: (Fire, Water, Earth, Air or other)
 Location: (Where can it be found) 
+Obtaining Method: (What is the method of obtaining the ingredient)
 Rarity: (How rare it is) 
 Uses: (What could it be used for in fantasy alchemy)"""
+
+basic()
