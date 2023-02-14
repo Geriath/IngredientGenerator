721c1500fdb2ea900f127bd27932385aa83c98ea
Geriath <34312983+Geriath@users.noreply.github.com>
2023-02-14 19:22:49 +0100

    Sure

--- app.py
+++ app.py
@@ -4,24 +4,21 @@ import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-@app.route("/", methods=("GET", "POST"))
-def index():
-    if request.method == "POST":
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt,
-            temperature=0.75,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
-
+def basic():
+    f = open("test.txt", "a")
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=generate_prompt,
+    temperature=0.75,
+)
+    f.write(response)
+    f.close()
 
 def generate_prompt():
     return """Generate a fictional fantasy alchemy ingredient based on the real world plants, animals, fungi or minerals. 
 Name: (The name for the ingredient) 
-Latin Name:
+Latin Name: (The name of the ingredient in latin)
 Description: (What it looks like) 
 Location: (Where can it be found) 
 Rarity: (How rare it is) 
